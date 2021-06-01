b62a4ba6b01c59f4bc8c1802316fe01b4d384cc7
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-06-01 23:20:53 +0300

    complete

--- contracts/EIP897proxy.sol
+++ contracts/EIP897proxy.sol
@@ -38,5 +38,21 @@ contract ProxyNoMoreClash is ProxyStorage {
      */
     fallback() external payable {
         address _impl = otherContractAddress;
+
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
+
+            switch result
+                case 0 {
+                    revert(ptr, size)
+                }
+                default {
+                    return(ptr, size)
+                }
+        }
     }
 }
